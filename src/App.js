bf2cec358dd8910a4700bfa7b6b1d389297193eb
torsumkhan <torsum.khan@gmail.com>
2022-06-08 01:11:50 +0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Memory Game</h3>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
